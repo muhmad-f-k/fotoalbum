73ccefc70b43336a7d2f07717134de505bad767f
Muhmad K. Fauzi <muhmad@live.no>
2022-03-22 23:19:37 +0100

    added

--- app/src/main/java/com/example/fotoalbum/MainActivity.kt
+++ app/src/main/java/com/example/fotoalbum/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.fotoalbum
-
+import kotlinx.android.synthetic.main.activity_main.recyclerView
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private var layoutManager: RecyclerView.LayoutManager? =null
+    private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+        adapter = RecyclerAdapter()
+        recyclerView.adapter=adapter
     }
 }
